ce2038e180ba3d1d2b2b48d037a097824637a474
acinlee <leedongjun181027@gmail.com>
2024-03-11 23:20:47 +0900

    이벤트 이해를 위한 샘플 파일 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
 import './App.css';
-import ClassComponent from "./ClassComponent";
-import ArrowComponent from "./ArrowComponent";
+import EventListener from './pages/EventListener';
+import OnClick from './pages/OnClick';
+import ReactOnClick from './pages/ReactOnClick';
+import DispatchEvent from './pages/DispatchEvent';
+import EventBubbling from './pages/EventBubbling';
+import StopPropagation from './pages/StopPropagation';
+import VariousInputs from './pages/VariousInputs';
+import OnChange from './pages/OnChange';
+import FileInput from './pages/FileInput';
+import DragDrop from './pages/DragDrop';
+import FileDrop from './pages/FileDrop';
+
 
 // 기본 함수형
 // function App() {
@@ -13,12 +23,28 @@ import ArrowComponent from "./ArrowComponent";
 // }
 
 // 화살표 방식 함수형
-const App = () => {
+// const App = () => {
+//     return (
+//         <ul>
+//             <ClassComponent href="https://naver.com" text="go to naver"/>
+//             <ArrowComponent href={"https://daum.net"} text={"go to daum!!"}/>
+//         </ul>
+//     )
+// }
+export default function App() {
     return (
-        <ul>
-            <ClassComponent href="https://naver.com" text="go to naver"/>
-            <ArrowComponent href={"https://daum.net"} text={"go to daum!!"}/>
-        </ul>
+        <div>
+            <FileDrop/>
+            <DragDrop/>
+            <FileInput/>
+            <OnChange/>
+            <VariousInputs/>
+            <StopPropagation/>
+            <EventBubbling/>
+            <DispatchEvent/>
+            <ReactOnClick/>
+            <OnClick/>
+            <EventListener/>
+        </div>
     )
 }
-export default App
